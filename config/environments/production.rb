603188edc905e716a25096b18c7137c764f08292
BryanL2303 <bryanlee0033@gmail.com>
2022-09-13 16:29:14 +0800

    commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
   config.serve_static_assets = true
 
   # Compress CSS using a preprocessor.
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sassc
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
