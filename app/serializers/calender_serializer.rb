1858059530278cc76378041342404dda9ab31d05
BryanL2303 <bryanlee0033@gmail.com>
2022-09-16 21:44:52 +0800

    commit

--- app/serializers/calender_serializer.rb
+++ app/serializers/calender_serializer.rb
@@ -2,5 +2,5 @@ class CalenderSerializer
   include FastJsonapi::ObjectSerializer
   attributes :date
 
-  has_many[:tasks]
+  has_many [:tasks]
 end
--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -2,5 +2,5 @@ class ProjectSerializer
   include FastJsonapi::ObjectSerializer
   attributes :project_name, :project_description, :tag
 
-  has_many[:tasks]
+  has_many :tasks
 end
